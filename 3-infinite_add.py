7c1cc99fccd88799c25d97b2ed1c45941cb24a37
FessorX <profaimusa@gmail.com>
2023-05-13 00:52:29 +0100

    more

--- 3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    count = len(argv)
-    sum = 0
-    for i in range(1, count):
-        sum += argv[i]
-    print(sum)
--- 4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-
-if __name__ == "__main__":
-    file = dir(hidden_4)
-    count = len(file)
-    for i in range(0, count):
-        if file[i][0:2] != "__":
-            print(file[i])
